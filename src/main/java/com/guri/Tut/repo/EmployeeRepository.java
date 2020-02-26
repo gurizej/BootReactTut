83f45ffb3332bfe29681cf3fc4581f0b5e5fc51a
Guri Zejnullahi <guri@Guris-Mac-mini.local>
2020-02-26 00:37:34 -0500

    First commit

---
+++ src/main/java/com/guri/Tut/repo/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.guri.Tut.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+}
---
+++ src/test/java/com/guri/Tut/TutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guri.Tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

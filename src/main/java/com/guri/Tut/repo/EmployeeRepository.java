8e94834a5c77afd64a8c127102dc6b9a6de2fa26
Guri Zejnullahi <guri@Guris-Mac-mini.local>
2020-02-26 16:44:54 -0500

    at part 3 of tutorial, pop up for creation is not working

--- src/main/java/com/guri/Tut/repo/EmployeeRepository.java
+++ src/main/java/com/guri/Tut/repo/EmployeeRepository.java
@@ -1,7 +1,8 @@
 package com.guri.Tut.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 
 }
